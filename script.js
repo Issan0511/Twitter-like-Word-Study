14314cb1bd40df9c83efb1a2726e23c8f35c214e
Issan0511 <143970263+Issan0511@users.noreply.github.com>
2023-10-04 22:23:47 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function shuffle(array) {
 
 let unknownWords = [];
 let allWords = [];
-let displayButton = document.getElementById('displayButton');
 
+// ファイル選択イベント
 document.getElementById('excelFileInput').addEventListener('change', function(e){
   allWords = [];
   const files = e.target.files;
@@ -35,22 +35,21 @@ document.getElementById('excelFileInput').addEventListener('change', function(e)
 
     reader.onloadend = function() {
       if (fileCount === 0) {
-        displayButton.disabled = false;
+        shuffle(allWords);
+        displayTweets(allWords);
+        displayTweets(allWords, false);
+        allWords = [];
       }
     };
   });
 });
 
-displayButton.addEventListener('click', function() {
-  shuffle(allWords);
-  displayTweets(allWords);
-  allWords = [];
-});
-
-function displayTweets(wordList) {
-  document.getElementById('timeline').innerHTML = '';
-
+// ツイートを表示する関数
+function displayTweets(wordList,append=false) {
   const timeline = document.getElementById('timeline');
+  if (!append) {
+    timeline.innerHTML = ''; // 既存のツイートをクリア
+  }
   wordList.forEach((item) => {
     const tweet = document.createElement('div');
     tweet.className = 'tweet';
@@ -74,6 +73,7 @@ function displayTweets(wordList) {
         this.textContent = '♥️';
         this.style.color = 'red';
         unknownWords.push(item);
+        displayTweets([item], true); 
       } else {
         this.textContent = '♡';
         this.style.color = 'black';
@@ -91,64 +91,4 @@ function displayTweets(wordList) {
 
     timeline.appendChild(tweet);
   });
-  const reviewButton = document.createElement('button');
-  reviewButton.textContent = 'さらに表示';
-  reviewButton.addEventListener('click', function() {
-    appendTweets(unknownWords);
-    unknownWords = [];
-  });
-  timeline.appendChild(reviewButton);
-
 }
-function appendTweets(wordList) {
-  const timeline = document.getElementById('timeline');
-  wordList.forEach((item) => {
-    const tweet = document.createElement('div');
-    tweet.className = 'tweet';
-
-    const icon = document.createElement('div');
-    icon.className = 'icon';
-
-    const accountName = document.createElement('span');
-    accountName.className = 'account-name';
-    accountName.textContent = 'Account Name';
-
-    const content = document.createElement('div');
-    content.className = 'content';
-    content.innerHTML = `<span class="word">${item.word}</span>:${item.answer}`;
-
-    const heart = document.createElement('span');
-    heart.className = 'heart';
-    heart.textContent = '♡';
-    heart.addEventListener('click', function() {
-      if (this.textContent === '♡') {
-        this.textContent = '♥️';
-        this.style.color = 'red';
-        unknownWords.push(item);
-      } else {
-        this.textContent = '♡';
-        this.style.color = 'black';
-        const index = unknownWords.indexOf(item);
-        if (index > -1) {
-          unknownWords.splice(index, 1);
-        }
-      }
-    });
-
-    tweet.appendChild(icon);
-    tweet.appendChild(accountName);
-    tweet.appendChild(content);
-    tweet.appendChild(heart);
-
-    timeline.appendChild(tweet);
-  });
-
-  const reviewButton = document.createElement('button');
-  reviewButton.textContent = 'さらに表示';
-  reviewButton.addEventListener('click', function() {
-    appendTweets(unknownWords);
-    unknownWords = [];
-  });
-  timeline.appendChild(reviewButton);
-}
-
